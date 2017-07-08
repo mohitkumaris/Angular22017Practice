25f46f807e449280b37e5deee7b0fd66e821d70d
mohitkumaris <mohitkumaris@gmail.com>
2017-07-08 21:13:11 +0530

    To show in grid

--- app/event/event-thumbnail.component.ts
+++ app/event/event-thumbnail.component.ts
@@ -5,15 +5,16 @@ import {Component, EventEmitter, Input, Output} from "@angular/core";
 
 @Component({
     selector:'event-thumbnail',
-    template:`<div>
-        <h1>Upcoming Angular Events</h1>
-        <hr>
+    template:`
+        
         <div class="well hoverwell thumbnail">
             <h2>{{event.name}}</h2>
             <div>Date: {{event.date}}</div>
+            <div>Price: \${{event.price}}</div>
+            <div>Time: {{event.time}}</div>
         </div>
         <button class="btn btn btn-primary" (click)="handleClickMe()">Click Me! </button>
-    </div>`
+    `
 })
 
 export class EventThumbnailComponent{
