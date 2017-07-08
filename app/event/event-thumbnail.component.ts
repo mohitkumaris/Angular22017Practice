2ab61c5bc67d802a341d53bb9ae7feb203935871
mohitkumaris <mohitkumaris@gmail.com>
2017-07-08 22:25:05 +0530

    code segration

--- app/event/event-thumbnail.component.ts
+++ app/event/event-thumbnail.component.ts
@@ -5,26 +5,17 @@ import {Component, EventEmitter, Input, Output} from "@angular/core";
 
 @Component({
     selector:'event-thumbnail',
-    template:`
-        
-        <div class="well hoverwell thumbnail">
-            <h2>{{event.name}}</h2>
-            <div>Date: {{event.date}}</div>
-            <div>Price: \${{event.price}}</div>
-            <div>Time: {{event.time}}</div>
-        </div>
-        <button class="btn btn btn-primary" (click)="handleClickMe()">Click Me! </button>
-    `
+    templateUrl:'./app/event/event-thumbnail.component.html'
 })
 
 export class EventThumbnailComponent{
 
    @Input() event:any;
-   @Output() eventClick=new EventEmitter();
+   /*@Output() eventClick=new EventEmitter();
 
     handleClickMe(){
         this.eventClick.emit();
-    }
+    }*/
 
 
 }
